0bcc317d5965f8fc8bb32dc255cd6767aec7aeb8
chengjhan <chengjhan@gmail.com>
2021-05-20 00:10:32 +0800

    first commit

---
+++ src/main/java/com/operaweb/OperaWebApplication.java
@@ -0,0 +1,13 @@
+package com.operaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperaWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/operaweb/OperaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
